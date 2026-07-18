cbfaeef714cc3594731ab657c5eabbddfe2c8659
anshuonline <helloanshu.dev@gmail.com>
2026-07-18 14:07:12 +0530

    Fix syntax error by parsing and executing SQL queries individually

--- sync_to_live.php
+++ sync_to_live.php
@@ -12,12 +12,26 @@ try {
         die("Error: live_data_import.sql not found!");
     }
     
-    $sql = file_get_contents($sql_file);
+    $sql_content = file_get_contents($sql_file);
     
-    // Execute the SQL dump
-    $db->exec($sql);
+    // Remove comments
+    $sql_content = preg_replace('/^-- .*$/m', '', $sql_content);
+    $sql_content = preg_replace('/^\/\*!.*\*\//m', '', $sql_content);
+    $sql_content = preg_replace('/^\/\*!.*;/m', '', $sql_content);
     
-    echo "<p style='color:green;font-weight:bold;'>Success! The database has been synced.</p>";
+    // Split into individual statements
+    $queries = explode(';', $sql_content);
+    
+    $success = 0;
+    foreach ($queries as $query) {
+        $query = trim($query);
+        if (!empty($query)) {
+            $db->exec($query);
+            $success++;
+        }
+    }
+    
+    echo "<p style='color:green;font-weight:bold;'>Success! Executed {$success} queries. The database has been synced.</p>";
     echo "<p>All 14 posts and categories are now live.</p>";
     echo "<p style='color:red;'>SECURITY WARNING: Please delete this file (sync_to_live.php) and live_data_import.sql from your Hostinger file manager immediately!</p>";
     echo "<a href='" . SITE_URL . "'>Go to Homepage</a>";
